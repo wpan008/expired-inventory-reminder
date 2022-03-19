e52899c1057c0ad63dc3f4bcbcd8653af4806854
Wei Pan <wei.pan@celohealth.com>
2022-03-19 20:48:37 +1300

    Updates

---
+++ precache-manifest.be16694389014ee7e5f7a09e32b96fb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "775af8850737e9eff7b7",
+    "url": "/expired-inventory-reminder/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "775af8850737e9eff7b7",
+    "url": "/expired-inventory-reminder/static/js/main.6d02acd5.chunk.js"
+  },
+  {
+    "revision": "114b19d357963a381b76",
+    "url": "/expired-inventory-reminder/static/js/runtime~main.92afa873.js"
+  },
+  {
+    "revision": "04ada5120ac143c8be5a",
+    "url": "/expired-inventory-reminder/static/css/2.6cb9063f.chunk.css"
+  },
+  {
+    "revision": "04ada5120ac143c8be5a",
+    "url": "/expired-inventory-reminder/static/js/2.7d2961ef.chunk.js"
+  },
+  {
+    "revision": "50c1ea85a415cd077ea9d3585f07f5e6",
+    "url": "/expired-inventory-reminder/index.html"
+  }
+];
